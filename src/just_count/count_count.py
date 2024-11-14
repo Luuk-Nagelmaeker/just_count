20d7e51b55972a17b0b984621e92c897a4628a38
Luuk-Nagelmaeker <ljbnagelmaeker@gmail.com>
2024-11-14 09:35:18 +0100

    fixed just count with command line interface

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,8 +1,12 @@
 import just_count.square
+import click
 
-def main():
+@click.command()
+@click.argument('integer')
+
+def main(integer):
     
-    print(f"The square of 5 is {just_count.square.square(5)}")
+    print(f"The square of {integer} is {just_count.square.square(int(integer))}")
 
 if __name__ == '__main__':
     main()
